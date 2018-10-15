2b20431bbaa5cad4612cf6892ba02de37e5a77b2
salmankr <44156508+salmankr@users.noreply.github.com>
2018-10-15 20:58:28 +0500

    Add files via upload

---
+++ database/migrations/2018_10_11_164109_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('semester');
+            $table->string('reg_no');
+            $table->string('img_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
