f1fe1478834ae87b9a0a4e7a59b825ea620580b0
salmankr <44156508+salmankr@users.noreply.github.com>
2018-10-16 03:03:27 -0700

    Add files via upload

--- app/Http/Controllers/createdataController.php
+++ app/Http/Controllers/createdataController.php
@@ -18,16 +18,23 @@ class createdataController extends Controller
     	]);
     	if ($request->hasFile('image')) {
     		$image = $request->file('image');
-    		$image_name = time().$image->getClientOriginalName();
-    		$image->move(public_path().'/images', $image_name);
+            $orignal_name = $image->getClientOriginalName();
+            $ext = $image->getClientOriginalExtension();
+    		$name_wt = time().$orignal_name;
+            $image_name = md5($name_wt).".".$ext;
+            if ($ext == 'png' or $ext == 'jpg' or $ext == 'jpeg') {
+                $image->move(public_path().'/images', $image_name);
+                $data = new student;
+                $data->name = $request->get('name');
+                $data->semester = $request->get('semester');
+                $data->reg_no = $request->get('reg_no');
+                $data->img_name = $image_name;
+                $data->save();
+                return redirect('/semester-category')->with('success', 'new student has been added');
+            }
+    		else{
+                return redirect('/add-new-student')->with('error', 'file type not allowed');
+            }
     	}
-        $data = new student;
-        $data->name = $request->get('name');
-        $data->semester = $request->get('semester');
-        $data->reg_no = $request->get('reg_no');
-        $data->img_name = $image_name;
-        $data->save();
-        return redirect('/semester-category')->with('success', 'new student has been added');
-
     }
 }
--- app/Http/Controllers/updatedataController.php
+++ app/Http/Controllers/updatedataController.php
@@ -11,22 +11,31 @@ class updatedataController extends Controller
         return view('crud.update', compact('student'));
     }
     public function updatedata(Request $request, $id){
-    	if ($request->hasFile('image')) {
-    		$image = $request->file('image');
-    		$image_name = time().$image->getClientOriginalName();
-    		$image->move(public_path().'/images', $image_name);
-    	}
     	$request->validate([
             'name' => 'required',
             'semester' => 'required',
             'reg_no' => 'required',
-    	]);
-    	$data = student::find($id);
-    	$data->name = $request->get('name');
-        $data->semester = $request->get('semester');
-        $data->reg_no = $request->get('reg_no');
-        $data->img_name = $image_name;
-        $data->save();
-        return redirect('/semester-category/students-list/'.$id)->with('success', 'Student data has been updated');
+        ]);
+        if ($request->hasFile('image')) {
+    		$image = $request->file('image');
+    		$orignal_name = $image->getClientOriginalName();
+            $ext = $image->getClientOriginalExtension();
+            $name_wt = time().$orignal_name;
+            $image_name = md5($name_wt).".".$ext;
+            if ($ext == 'png' or $ext == 'jpg' or $ext == 'jpeg') {
+                $image->move(public_path().'/images', $image_name);
+                $data = student::find($id);
+                $data->name = $request->get('name');
+                $data->semester = $request->get('semester');
+                $data->reg_no = $request->get('reg_no');
+                $data->img_name = $image_name;
+                $data->save();
+                return redirect('/semester-category/students-list/'.$id)->with('success', 'Student data has been updated');
+            }
+    		else{
+                return redirect('/semester-category/students-list/'.$id.'/update')->with('error', 'file type not allowed');
+            }
+    	}
+    	
     }
 }
